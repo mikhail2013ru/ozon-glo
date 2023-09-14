28d47071a26e13f78b3a86ff3f2ed05147363cce
mikhail2013ru <mikhail2013ru@gmail.com>
2023-09-14 23:59:14 +0300

    2 день

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,8 +1,27 @@
-const getData = () => {
-    return fetch('https://ozone-glo-70f5c-default-rtdb.firebaseio.com/goods.json')
-    .then((response) => {
-        return response.json()
-    })
+import Notify from 'simple-notify'
+
+const getData = (str) => {
+    return fetch(
+        `https://ozone-glo-70f5c-default-rtdb.firebaseio.com/goods.json?${str ? `search=${str}`: ''}`
+        )
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                throw new Error('Ошибка запроса')
+            }
+        })
+        .catch(error => {
+            new Notify({
+                status: 'error',
+                title: 'Ошибка!',
+                text: error.message,
+                effect: 'slide',
+                type: 3,
+                autoclose: true,
+            })
+
+        })
 }
 
 export default getData
